554a5000371ed161c959b543340ec95223c0d91a
yi <jcd4625@gmail.com>
2019-12-13 10:47:10 +0900

    application_coffee

--- dao/product_dao.py
+++ dao/product_dao.py
@@ -1,25 +1,47 @@
 from mysql.connector import Error
-
 from db_connection.db_connection import ConnectionPool
 
 
 class ProductDao():
-
     def __init__(self):
         pass
 
-    def select(self, sql):
+    def delete_product(self, sql, code):
         try:
             conn = ConnectionPool.get_instance().get_connection()
             cursor = conn.cursor()
-            cursor.execute(sql)
-            for row in self.iter_row(cursor, 5):
-                print(type(row), " ", row)
-        except Error as e:
-            print(e)
+            cursor.execute(sql, (code,))
+            conn.commit()
+        except Error as error:
+            print(error)
         finally:
             cursor.close()
             conn.close()
+
+    def __do_query(self, query=None, arg=None):
+        try:
+            conn = ConnectionPool.get_instance().get_connection()
+            cursor = conn.cursor()
+            cursor.execute(query, arg)
+            conn.commit()
+        except Error as error:
+            print(error)
+            raise error
+        finally:
+            cursor.close()
+            conn.close()
+
+    def update_product(self, sql, name, code):
+        args = (name, code)
+        try:
+            self.__do_query(query=sql, arg=args)
+            return True
+        except Error as e:
+            return False
+
+    def insert_product(self, sql, code, name):
+        args = (code, name)
+        self.__do_query(query=sql, arg=args)
 
     def __iter_row(self, cursor, size=5):
         while True:
@@ -29,3 +51,15 @@ class ProductDao():
             for row in rows:
                 yield row
 
+    def select(self, sql):
+        try:
+            conn = ConnectionPool.get_instance().get_connection()
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            for row in self.__iter_row(cursor, 5):
+                print(type(row), " ", row)
+        except Error as e:
+            print(e)
+        finally:
+            cursor.close()
+            conn.close()
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from dao.product_dao import ProductDao
+from db_connection.db_connection import ConnectionPool
+
+
 if __name__ == "__main__":
-    pass
+    pool = ConnectionPool.get_instance()
+    connection = pool.get_connection()
+
+    print(pool, connection)
+
+    pdt = ProductDao()
+    pdt.insert_product()
+    # pdt.delete_product()
+    # pdt.select()
+    # pdt.update_product()
